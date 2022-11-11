a81cc3ffcf1b4b15525c96734b5efd2fafa025de
anndyX <117907405+anndyX@users.noreply.github.com>
2022-11-11 16:38:10 +0800

    Update mailserver.py

--- mailserver.py
+++ mailserver.py
@@ -145,7 +145,58 @@ class Pop3:  #邮件接收类
         Socket.close()
         return 
 
-            
+class SQL:  # 数据存储类
+    @staticmethod
+    def create_sql(self):
+        try:
+            conn = pymysql.connect(host='localhost', user='root', password='123456',port=3306, charset='utf8')
+        except Exception as e:
+            print(f'数据库连接失败：{e}')
+        cursor = conn.cursor()
+        sql_init = 'create table  %s (sender char(20), receiver char(20), topic char(30), store_addr char(50) ' \
+                   'PRIMARY KEY(sender, receiver, topic))' % (GroupII,)
+        cursor.execute(sql_init)
+        cursor.close()
+        conn.close()
 
-#class SQL:  #数据存储类
+    @staticmethod
+    def search_sql(sender, receiver, topic):
+        try:
+            conn = pymysql.connect(host='localhost', user='root', password='123456', port=3306, charset='utf8')
+        except Exception as e:
+            print(f'数据库连接失败：{e}')
+        cursor = conn.cursor()
+        sql_search = 'select * from GroupII WHERE sender = %s AND receiver = %s AND topic = %s' \
+                     % (sender, receiver, topic)
+        cursor.execute(sql_search)
+        results = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        if not results:
+            return
+        return results
 
+    @staticmethod
+    def delete_sql(sender, receiver, topic):
+        try:
+            conn = pymysql.connect(host='localhost', user='root', password='123456', port=3306, charset='utf8')
+        except Exception as e:
+            print(f'数据库连接失败：{e}')
+        cursor = conn.cursor()
+        sql_delete = 'DELETE FROM user WHERE sender = %s AND receiver = %s AND topic = %s' % (sender, receiver, topic)
+        cursor.execute(sql_delete)
+        cursor.close()
+        conn.close()
+
+    @staticmethod
+    def add_sql(self, sender, receiver, topic, store_addr):
+        try:
+            conn = pymysql.connect(host='localhost', user='root', password='123456', port=3306, charset='utf8')
+        except Exception as e:
+            print(f'数据库连接失败：{e}')
+        cursor = conn.cursor()
+        sql_add = 'INSERT INTO GroupII VALUES (%s,%s,%s,%s,%s,%s)' % (sender, receiver, topic, store_addr)
+        cursor.execute(sql_add)
+        conn.commit()
+        cursor.close()
+        conn.close()
