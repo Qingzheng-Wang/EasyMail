093fb0a927dddc608ca8678f924be6c1106a13d9
Qingzheng Wang <QingzhengWang@whu.edu.cn>
2023-10-26 16:34:39 +0800

    Update MainWindow.py

--- MainWindow.py
+++ MainWindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'D:\2022-2023大三上学习笔记\计算机网络课程设计\Mail-System-Xuhao\MainWindow.ui'
+# Form implementation generated from reading ui file 'D:\2022-2023大三上学习笔记\计算机网络课程设计\Mail-System\MainWindow.ui'
 #
 # Created by: PyQt5 UI code generator 5.9.2
 #
